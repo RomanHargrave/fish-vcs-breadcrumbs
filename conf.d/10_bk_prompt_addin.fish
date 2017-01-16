8d4f66d62d6ba08e87cd7fe896e52473bb260f34
Roman Hargrave <roman@hargrave.info>
2017-01-16 16:58:16 -0600

    init

---
+++ conf.d/10_bk_prompt_addin.fish
@@ -0,0 +1,21 @@
+# Watch PWD to check for bitkeeper dir entry on dir change
+
+if isatty
+    function _bk_prompt.pwd_watcher -v PWD
+        if bk root >/dev/null ^/dev/null
+            fish_prompt_breadcrumb add _bk_prompt.bk_status_seg
+        else
+            fish_prompt_breadcrumb rm _bk_prompt.bk_status_seg
+        end
+    end
+
+    function _bk_prompt.bk_status_seg
+        set description (bk describe)
+        if string match -r '.+-dirty' $description >/dev/null
+            set_color red
+        else
+            set_color green
+        end
+        printf 'bk:%s' $description
+    end
+end
---
+++ conf.d/10_svn_prompt_addin.fish
@@ -0,0 +1,17 @@
+# Watch PWD to check for svn dir entry
+
+if isatty
+    function _svn_prompt.pwd_watcher -v PWD
+        if svn info >/dev/null ^/dev/null
+            fish_prompt_breadcrumb add _svn_prompt.svn_status_seg
+        else
+            fish_prompt_breadcrumb rm _svn_prompt.svn_status_seg
+        end
+    end
+end
+
+function _svn_prompt.svn_status_seg
+    set description (svn info --show-item revision)
+    set_color white
+    printf 'svn:%s' $description
+end
